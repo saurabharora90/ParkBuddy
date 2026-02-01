d5678d49506eab349c166b7386ef446d25c01d7d
Saurabh Arora <saurabharora90@gmail.com>
2026-01-31 23:21:16 -0800

    Add a broadcast receiver which listens to bluetooth disconnected broadcast
    We'll use this broadcast receiver as a trigget to fetch the user location, assume that is the locaiton when the car is being parked and then setup a reminder

---
+++ feature/reminders/src/main/java/dev/parkbuddy/feature/reminders/BluetoothConnectionReceiver.kt
@@ -0,0 +1,60 @@
+package dev.parkbuddy.feature.reminders
+
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import dev.bongballe.parkbuddy.DispatcherType
+import dev.bongballe.parkbuddy.data.repository.ParkingRepository
+import dev.bongballe.parkbuddy.data.repository.PreferencesRepository
+import dev.bongballe.parkbuddy.data.repository.ReminderRepository
+import dev.bongballe.parkbuddy.qualifier.WithDispatcherType
+import dev.bongballe.parkbuddy.qualifier.WithScope
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoMap
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.binding
+import dev.zacsweers.metrox.android.BroadcastReceiverKey
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+@ContributesIntoMap(AppScope::class, binding<BroadcastReceiver>())
+@BroadcastReceiverKey(BluetoothConnectionReceiver::class)
+@Inject
+class BluetoothConnectionReceiver(
+  private val preferencesRepository: PreferencesRepository,
+  private val parkingRepository: ParkingRepository,
+  private val reminderRepository: ReminderRepository,
+  @WithScope(AppScope::class) private val coroutineScope: CoroutineScope,
+  @WithDispatcherType(DispatcherType.IO) private val ioDispatcher: CoroutineDispatcher,
+) : BroadcastReceiver() {
+
+  override fun onReceive(context: Context, intent: Intent) {
+    if (intent.action == BluetoothDevice.ACTION_ACL_DISCONNECTED) {
+      val device =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice::class.java)
+        } else {
+          @Suppress("DEPRECATION") intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+        }
+
+      device?.let { disconnectedDevice ->
+        val pendingResult = goAsync()
+
+        coroutineScope.launch(ioDispatcher) {
+          try {
+            val savedAddress = preferencesRepository.bluetoothDeviceAddress.firstOrNull()
+            val isAutoTrackingEnabled =
+              preferencesRepository.isAutoTrackingEnabled.firstOrNull() ?: true
+
+          } finally {
+            pendingResult.finish()
+          }
+        }
+      }
+    }
+  }
+}
